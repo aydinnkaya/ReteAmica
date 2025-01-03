1c2b22fd560a9ac21e9dd9dc8ff9fd0c2d1b9d48
aydinnkaya <aydinnkaya1@gmail.com>
2025-01-03 11:19:45 +0300

    MVVM Created

--- ReteAmica/Data/Repo/PersonDaoRepository.swift
+++ ReteAmica/Data/Repo/PersonDaoRepository.swift
@@ -6,9 +6,12 @@
 //
 
 import Foundation
+import RxSwift
 
 
 class PersonDaoRepository {
+    
+    var personList = BehaviorSubject<[Kisiler]>(value: [Kisiler]())
     
     func personSave(kisi_ad:String, kisi_tel:String){
         print("Kisi ad: \(kisi_ad), Kisi tel: \(kisi_tel)")
@@ -17,6 +20,25 @@ class PersonDaoRepository {
     
     func personUpdate(kisi_id: Int, kisi_ad:String, kisi_tel:String){
         print("Kisi id: \(kisi_id), Kisi ad: \(kisi_ad), Kisi tel: \(kisi_tel)")
+    }
+    
+    func personDelete(kisi_id: Int){
+        
+    }
+    
+    func personSearch(inputText: String){
+        
+    }
+    
+    func personLoading(){
+        var list = Kisiler()
+        let initialList = [
+            Kisiler(kisi_id: 1, kisi_ad: "Aydin", kisi_tel: "5314681288"),
+            Kisiler(kisi_id: 2, kisi_ad: "Kelebek", kisi_tel: "5314681288"),
+            Kisiler(kisi_id: 3, kisi_ad: "Bebis", kisi_tel: "5314681288"),
+            Kisiler(kisi_id: 4, kisi_ad: "Hatun", kisi_tel: "5314681288")
+        ]
+        personList.onNext(initialList)
     }
     
 }
